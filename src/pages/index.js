f63f2ecb3424282bfb6ce1d3f86d4b79527774fb
Yandra Muslim <yandrayt1@gmail.com>
2023-07-07 18:22:16 +0700

    feat: :zap: Add routing for checkout and index.js in pages

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Checkout from "./checkout";
+import Home from "./home";
+import Login from "./login";
+import Products from "./products";
+import Register from "./register";
+
+export { Checkout, Home, Login, Products, Register };
