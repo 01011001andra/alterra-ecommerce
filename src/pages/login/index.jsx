48962b2b60d0dcda89521be685f5fec7185905a0
putrapuices <puices14@gmail.com>
2023-07-16 20:42:33 +0800

    login dan register

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import store from '../../store/indexlogin';
 
-export default function Login() {
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Proses login menggunakan email dan password yang tersimpan
+    console.log('Email:', email);
+    console.log('Password:', password);
+    dispatch({ type: 'SET_EMAIL', payload: email });
+    dispatch({ type: 'SET_PASSWORD', payload: password });
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <div className="form-group">
+        <label>Email</label>
+        <input
+          type="email"
+          className="form-control"
+          placeholder="Masukkan email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Masukkan password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">Login</button>
+    </form>
+  );
+};
+
+export default Login;
--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import store from '../../store/indexlogin';
 
-export default function register() {
+const Register = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Proses penyimpanan data register
+    if (email && password) {
+      // Lakukan proses penyimpanan data register di sini
+      setMessage('Pendaftaran berhasil!');
+      dispatch({ type: 'SET_EMAIL', payload: email });
+      dispatch({ type: 'SET_PASSWORD', payload: password });
+    } else {
+      setMessage('Email dan password harus diisi!');
+    }
+  };
+
   return (
-    <div>register</div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <h2>Register</h2>
+      <div className="form-group">
+        <label>Email</label>
+        <input
+          type="email"
+          className="form-control"
+          placeholder="Masukkan email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Masukkan password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">Register</button>
+      {message && <p>{message}</p>}
+    </form>
+  );
+};
+
+export default Register;
